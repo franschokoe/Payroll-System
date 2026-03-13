9c1b7694c8de086eb1638bf6f26951b6622f79ae
Frans Chokoe <fmoyagabo@gmail.com>
2026-03-13 17:43:54 +0200

    Declarign variables

--- src/InteractivePayroll.java
+++ src/InteractivePayroll.java
@@ -1,7 +1,21 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class InteractivePayroll {
     public static void main(String [] args){
+        //Initialize vairables
+        Scanner scanner = new Scanner(System.in);
+        String employeeName = JOptionPane.showInputDialog(
+                null,
+                "Enter your name",
+                "Employee Name",
+                JOptionPane.INFORMATION_MESSAGE
+        );
+        String employeeNameScanner = scanner.nextLine();
+
+        int employeeId;
+        boolean paymentType;
+
 
     }
 }
