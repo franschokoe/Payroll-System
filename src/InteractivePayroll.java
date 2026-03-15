d4e32fa6bbf8ae9b1693553cf716e597e9e6f580
Frans Chokoe <fmoyagabo@gmail.com>
2026-03-15 22:42:55 +0200

    Console:Adding it 3

--- src/InteractivePayroll.java
+++ src/InteractivePayroll.java
@@ -32,186 +32,203 @@ public class InteractivePayroll {
 //        String employeeNameScanner = scanner.nextLine();
 
         //runnign with while
+        Scanner scanner = new Scanner(System.in);
+        int userDecision = JOptionPane.showConfirmDialog(
+                null,
+                "Select Input Method:\nYes.Console. No.GUI.",
+                "Payroll",
+                JOptionPane.YES_NO_OPTION
+        );
+        if (userDecision==JOptionPane.YES_OPTION){
+            boolean active = true;
+            while(active){
+                //Initialize vairables
 
-        boolean active = true;
-        while(active){
-            //Initialize vairables
-            Scanner scanner = new Scanner(System.in);
-            String employeeName = JOptionPane.showInputDialog(
-                    null,
-                    "Enter your full names",
-                    "Employee Name",
-                    JOptionPane.QUESTION_MESSAGE
-            );
+                String employeeName = JOptionPane.showInputDialog(
+                        null,
+                        "Enter your full names",
+                        "Employee Name",
+                        JOptionPane.QUESTION_MESSAGE
+                );
 
-            String employeeId = JOptionPane.showInputDialog(
-                    null,
-                    "Enter your id",
-                    "Employeee Id",
-                    JOptionPane.QUESTION_MESSAGE
-                    //if statement to control the digits entered
-            );
+                String employeeId = JOptionPane.showInputDialog(
+                        null,
+                        "Enter your id",
+                        "Employeee Id",
+                        JOptionPane.QUESTION_MESSAGE
+                        //if statement to control the digits entered
+                );
 
-            int paymentType = JOptionPane.showConfirmDialog(
-                    null,
-                    "Is your payment in a form of a salary?",
-                    "Payment Type",
-                    JOptionPane.YES_NO_OPTION
-                    //if yes the we continue to salary calculation
-                    //with no we contnue to hourly calculation
-            );
+                int paymentType = JOptionPane.showConfirmDialog(
+                        null,
+                        "Is your payment in a form of a salary?",
+                        "Payment Type",
+                        JOptionPane.YES_NO_OPTION
+                        //if yes the we continue to salary calculation
+                        //with no we contnue to hourly calculation
+                );
 
-            try {
-                double userMonthlySalary = 0.00;
-                double userBiweeklySalary = 0.00;
-                double userweeklySalary = 0.00;
-                double employeeWage = 0.00;
-                double totalHoursWage = 0.00;
-                if (paymentType == JOptionPane.YES_OPTION) {
-                    //user enter their salary here
-                    try {
-                        double employeeSalary = Double.parseDouble(JOptionPane.showInputDialog(
-                                null,
-                                "Enter Your annual salary:",
-                                "Employee Salary",
-                                JOptionPane.QUESTION_MESSAGE
+                try {
+                    double userMonthlySalary = 0.00;
+                    double userBiweeklySalary = 0.00;
+                    double userweeklySalary = 0.00;
+                    double employeeWage = 0.00;
+                    double totalHoursWage = 0.00;
+                    if (paymentType == JOptionPane.YES_OPTION) {
+                        //user enter their salary here
+                        try {
+                            double employeeSalary = Double.parseDouble(JOptionPane.showInputDialog(
+                                    null,
+                                    "Enter Your annual salary:",
+                                    "Employee Salary",
+                                    JOptionPane.QUESTION_MESSAGE
 
-                        ));
-                        //input scanner for double
-                        //double userSalary = scanner.nextDouble();
+                            ));
+                            //input scanner for double
+                            //double userSalary = scanner.nextDouble();
 
-                        //calculating monthly bi-weekly , weekly salary
-                        userMonthlySalary = (employeeSalary / 12);
-                        userBiweeklySalary = (userMonthlySalary / 31) * 14;
-                        userweeklySalary = (userMonthlySalary / 31) * 7;
+                            //calculating monthly bi-weekly , weekly salary
+                            userMonthlySalary = (employeeSalary / 12);
+                            userBiweeklySalary = (userMonthlySalary / 31) * 14;
+                            userweeklySalary = (userMonthlySalary / 31) * 7;
 
-                    } catch (NumberFormatException e) {
-                        //null for not number input
-                        JOptionPane.showMessageDialog(
-                                null,
-                                "Invalid annual salary number only",
-                                "Error ",
-                                JOptionPane.ERROR
-                        );
-                        break;
-                    } catch (NullPointerException e) {
-                        //error for null input
-                        JOptionPane.showMessageDialog(
-                                null,
-                                "Please enter some value",
-                                "Error",
-                                JOptionPane.ERROR
-                        );
-                        break;
-                    }
-
-                } else if (paymentType == JOptionPane.NO_OPTION) {
-                    // hourly employee sections
-                    try {
-                        double employeeWorkHour = Double.parseDouble(JOptionPane.showInputDialog(
-                                null,
-                                "Enter your woking hours",
-                                "Employee Hours",
-                                JOptionPane.QUESTION_MESSAGE
-                        ));
-                        double employeeHourRate = Double.parseDouble(JOptionPane.showInputDialog(
-                                null,
-                                "Enter your hourly rate",
-                                "Employee Hours",
-                                JOptionPane.QUESTION_MESSAGE
-                        ));
-                        // checking the number of hours to detect the extra hours
-                        final int normalHours = 40;
-                        if (employeeWorkHour == normalHours) {
-                            //normal hours = 40
-                            employeeWage = employeeWorkHour * employeeHourRate;
-
-                        } else if (employeeWorkHour > normalHours) {
-                            double extraTimeRate = 1.5;
-                            //finding the difference
-                            double extraHours = employeeWorkHour - normalHours;
-                            //final etra hours amount
-                            double extraHoursWage = extraHours * extraTimeRate; //output this
-                            //total extra time earned
-                            totalHoursWage = extraHoursWage * employeeHourRate;
-                            employeeWage = employeeWorkHour * employeeHourRate; // for the output
-                        }
-                        else if(employeeWorkHour <normalHours){
+                        } catch (NumberFormatException e) {
+                            //null for not number input
                             JOptionPane.showMessageDialog(
                                     null,
-                                    "No hours less than 40 hours\ntry again",
-                                    "Hourly Error",
-                                    JOptionPane.ERROR_MESSAGE
+                                    "Invalid annual salary number only",
+                                    "Error ",
+                                    JOptionPane.ERROR
                             );
-                        }
-                        else {
+                            break;
+                        } catch (NullPointerException e) {
+                            //error for null input
                             JOptionPane.showMessageDialog(
                                     null,
-                                    "Error orcured start",
+                                    "Please enter some value",
                                     "Error",
-                                    JOptionPane.ERROR_MESSAGE
+                                    JOptionPane.ERROR
                             );
                             break;
                         }
-                    } catch (NullPointerException e) {
-                        //catch for hours
-                        JOptionPane.showMessageDialog(
-                                null,
-                                "INvalid Error enter something",
-                                "Hourly Errors",
-                                JOptionPane.ERROR
-                        );
+
+                    } else if (paymentType == JOptionPane.NO_OPTION) {
+                        // hourly employee sections
+                        try {
+                            double employeeWorkHour = Double.parseDouble(JOptionPane.showInputDialog(
+                                    null,
+                                    "Enter your woking hours",
+                                    "Employee Hours",
+                                    JOptionPane.QUESTION_MESSAGE
+                            ));
+                            double employeeHourRate = Double.parseDouble(JOptionPane.showInputDialog(
+                                    null,
+                                    "Enter your hourly rate",
+                                    "Employee Hours",
+                                    JOptionPane.QUESTION_MESSAGE
+                            ));
+                            // checking the number of hours to detect the extra hours
+                            final int normalHours = 40;
+                            if (employeeWorkHour == normalHours) {
+                                //normal hours = 40
+                                employeeWage = employeeWorkHour * employeeHourRate;
+
+                            } else if (employeeWorkHour > normalHours) {
+                                double extraTimeRate = 1.5;
+                                //finding the difference
+                                double extraHours = employeeWorkHour - normalHours;
+                                //final etra hours amount
+                                double extraHoursWage = extraHours * extraTimeRate; //output this
+                                //total extra time earned
+                                totalHoursWage = extraHoursWage * employeeHourRate;
+                                employeeWage = employeeWorkHour * employeeHourRate; // for the output
+                            }
+                            else if(employeeWorkHour <normalHours){
+                                JOptionPane.showMessageDialog(
+                                        null,
+                                        "No hours less than 40 hours\ntry again",
+                                        "Hourly Error",
+                                        JOptionPane.ERROR_MESSAGE
+                                );
+                            }
+                            else {
+                                JOptionPane.showMessageDialog(
+                                        null,
+                                        "Error orcured start",
+                                        "Error",
+                                        JOptionPane.ERROR_MESSAGE
+                                );
+                                break;
+                            }
+                        } catch (NullPointerException e) {
+                            //catch for hours
+                            JOptionPane.showMessageDialog(
+                                    null,
+                                    "INvalid Error enter something",
+                                    "Hourly Errors",
+                                    JOptionPane.ERROR
+                            );
+                        }
+                    }
+                    //results section in a formated way
+                    String summaryResults = String.format(
+                            "Full Names: %s\n " +
+                                    "Employee ID: %s\n\n " +
+                                    "----------------------------\n\n" +
+                                    "SALARY EMPLOYEE\n\n" +
+                                    "Monthly Salary:R %.2f\n " +
+                                    "Bi-Weekly Salary:R %.2f\n " +
+                                    "Week Salary:R %.2f\n\n " +
+                                    "-------------------------------\n\n" +
+                                    "HOURLY EMPLOYEE\n\n" +
+                                    "Weekly amount(normal hours):R %.2f\n " +
+                                    "Overtime amount:R %.2f ",
+                            employeeName,
+                            employeeId,
+                            userMonthlySalary,
+                            userBiweeklySalary,
+                            userweeklySalary,
+                            employeeWage,
+                            totalHoursWage
+                    );
+
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "" + summaryResults,
+                            "Sumary Results",
+                            JOptionPane.INFORMATION_MESSAGE
+
+                    );
+
+                }
+                catch (NumberFormatException e){
+                    System.out.print("Error in first try" + e);
+                    break;
+                }
+                finally {
+                    //repetetive
+                    int Decision = JOptionPane.showConfirmDialog(
+                            null ,
+                            "Do you want to continue ?",
+                            "Ending",
+                            JOptionPane.YES_NO_OPTION
+                    );
+                    if (Decision==JOptionPane.NO_OPTION){
+                        break;
+                    } else if (Decision == JOptionPane.YES_OPTION) {
+                        continue;
                     }
                 }
-                //results section in a formated way
-                String summaryResults = String.format(
-                                "Full Names: %s\n " +
-                                "Employee ID: %s\n\n " +
-                                "----------------------------\n\n" +
-                                "SALARY EMPLOYEE\n\n" +
-                                "Monthly Salary:R %.2f\n " +
-                                "Bi-Weekly Salary:R %.2f\n " +
-                                "Week Salary:R %.2f\n\n " +
-                                "-------------------------------\n\n" +
-                                "HOURLY EMPLOYEE\n\n" +
-                                "Weekly amount(normal hours):R %.2f\n " +
-                                "Overtime amount:R %.2f ",
-                        employeeName,
-                        employeeId,
-                        userMonthlySalary,
-                        userBiweeklySalary,
-                        userweeklySalary,
-                        employeeWage,
-                        totalHoursWage
-                );
-
-                JOptionPane.showMessageDialog(
-                        null,
-                        "" + summaryResults,
-                        "Sumary Results",
-                        JOptionPane.INFORMATION_MESSAGE
-
-                );
-
             }
-            catch (NumberFormatException e){
-                System.out.print("Error in first try" + e);
-                break;
-            }
-            finally {
-                //repetetive
-                int Decision = JOptionPane.showConfirmDialog(
-                        null ,
-                        "Do you want to continue ?",
-                        "Ending",
-                        JOptionPane.YES_NO_OPTION
-                );
-                if (Decision==JOptionPane.NO_OPTION){
-                    break;
-                } else if (Decision == JOptionPane.YES_OPTION) {
-                    continue;
-                }
-            }
+        }else if(userDecision==JOptionPane.NO_OPTION){
+            //console input
+            System.out.print("Enter your full name: ");
+            String name = scanner.nextLine();
+            System.out.print("Enter your ID: ");
+            String Id = scanner.nextLine();
+            System.out.print("Do you earn in Salary or hours:(Y=Salary | N=Hourly): ");
+            String pamentmethods = scanner.nextLine();
         }
+
     }
 }
